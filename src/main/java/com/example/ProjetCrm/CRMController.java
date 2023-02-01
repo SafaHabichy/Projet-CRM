3b6e122f5f06c2617830b0769fb6b0a7063c6c04
Habichy Safa <safahabichy@gmail.com>
2023-02-01 10:19:11 +0100

    first commit

--- src/main/java/com/example/ProjetCrm/CRMController.java
+++ src/main/java/com/example/ProjetCrm/CRMController.java
@@ -8,21 +8,25 @@ import java.util.List;
 
 @RestController
 @RequestMapping("Api")
+@CrossOrigin
 public class CRMController {
 
     @Autowired
     CRMService crmService;
 
+    @CrossOrigin
     @PostMapping("clients")
     public void createClient(@RequestBody Client client){
         crmService.createClient(client);
     }
 
+    @CrossOrigin
     @PostMapping("orders")
     public void createOrder(@RequestBody Order order){
         crmService.createOrder(order);
     }
 
+    @CrossOrigin
     @GetMapping("clients")
     public List<ClientDTO> getClients(){
         List<Client> clients = crmService.getClients();
@@ -32,7 +36,7 @@ public class CRMController {
         }
         return dtos;
     }
-
+    @CrossOrigin
     @GetMapping("orders")
     public List<OrderDTO> getOrders(){
         List<Order> orders = crmService.getOrders();
@@ -42,21 +46,21 @@ public class CRMController {
         }
         return dtos;
     }
-
+    @CrossOrigin
     @DeleteMapping("clients/{id}")
     public void deleteClient(@PathVariable Integer id){
         crmService.deleteClient(id);
     }
-
+    @CrossOrigin
     @DeleteMapping("orders/{id}")
     public void deleteOrder(@PathVariable Integer id){
         crmService.deleteOrder(id);
     }
-
+    @CrossOrigin
     @PutMapping("clients/{id}")
     public void updateClient(@PathVariable Integer id, @RequestBody Client client){
         crmService.updateClient(id, client);}
-
+    @CrossOrigin
     @PutMapping("orders/{id}")
     public void updateOrder(@PathVariable Integer id, @RequestBody Order order){
         crmService.updateOrder(id, order);
